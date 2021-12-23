f05ae570bc26fbe136266a437d232dcd54531286
sohailmahmud <sohailmahmuud@gmail.com>
2021-12-23 23:08:07 +0600

    Create covariant.dart

---
+++ covariant.dart
@@ -0,0 +1,23 @@
+
+class Person {
+  final String name;
+  const Person(this.name);
+  @override
+  bool operator ==(Object other) {
+    if (other is! Person) throw ArgumentError('Was expecting a person');
+    return other.name == name;
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+}
+
+class Person {
+  final String name;
+  const Person(this.name);
+  @override
+  bool operator ==(covariant Person other) => other.name == name;
+
+  @override
+  int get hashCode => name.hashCode;
+}
